23b20b9d7bab12b4591e31299f6c3a797cd5a53d
ICHIGO <peepsqueakyface@gmail.com>
2025-01-14 22:52:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ import axios from 'axios'
 
 // Global variables
 global.owner = [
-  ['918360234087', 'Ｗ Ｈ Ｉ Ｔ Ｅ ４ ４ ４  ? ?', true], 
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['233533763772', 'Abraham Dwamena2.0', true],
-  ['233268374753', 'Abraham Dwamena3.0', true]
+  ['27688338288', '𝙺𝙴ͥ𝙽ͣ𝚃ᷟ𝙴𝙽𝚂𝙷𝙸⍣楤⁩', true], 
+  ['27695815823', '⁨𝙄𝘾𝙃𝙄𝙂𝙊☾⁩ -(5PHESIHLE)', true], 
+  ['27710200228', 'OLD NUMBER', true],
+  ['233268374753', 'IDEK THIS GUY.0', true]
 ] 
 
-global.mods = ['917888755628','923184070915', '233268374753', '923184474176', '233268374753'] 
-global.prems = ['923184070915', '94789481495', '918360234087', '233268374753']
-global.allowed = ['923184070915', '233268374753', '918360234087', '233268374753']
+global.mods = ['27695815823','27688338288', '233268374753', '923184474176', '233268374753'] 
+global.prems = ['27695815823', '27688338288', '918360234087', '233268374753']
+global.allowed = ['27695815823', '27688338288', '918360234087', '233268374753']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -49,20 +49,20 @@ global.APIKeys = {
 }
 
 // Sticker WM
-global.botname = '𝘟𝘭𝘪𝘤𝘰𝘯 𝘝𝘰𝘭 2'
+global.botname = 'GETSUGA TENSHOU'
 global.premium = 'true'
-global.packname = '𝙏𝙚𝙖𝙢 𝙓𝙇𝙄𝘾𝙊𝙉' 
-global.author = '@𝘮𝘥𝘹𝘭𝘪𝘤𝘰𝘯𝘷2' 
-global.menuvid = 'https://files.catbox.moe/yu3j54.mp4'
-global.igfg = '▢✓ Follow Our channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
-global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
-global.fgsc = 'https://github.com/salmanytofficial/XLICON-V2-MD' 
-global.fgyt = 'https://youtube.com/@s4salmanyt'
-global.fgpyp = 'https://youtube.com/@s4salmanyt'
+global.packname = '𝙏𝙚𝙖𝙢 GETSUGA' 
+global.author = '@ESPADATECHINFO' 
+global.menuvid = 'https://files.catbox.moe/va51uv.mp4'
+global.igfg = '▢✓ Follow Our channel\nhttps://whatsapp.com/channel/0029Vayxd786mYPMFMVB9A3Y\n' 
+global.dygp = 'https://chat.whatsapp.com/CicqD04sNCJ37j13LiI51p'
+global.fgsc = 'https://github.com/EspadaTechinfo/GETSUGA-XMD' 
+global.fgyt = 'https://youtube.com/@Itachisimp'
+global.fgpyp = 'https://youtube.com/@Itachisimp'
 global.fglog = 'XLICON.jpg' 
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
-global.wait = '*🕣 _XLICON IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 _GETSUGA IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✔️'
